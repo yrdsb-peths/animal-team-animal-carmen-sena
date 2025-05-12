110fe34981f4f6d865fb6ca1ea7a68d0dd8b4925
CC-iam <1202manman@gmail.com>
2025-05-12 14:24:59 -0400

    Update MyWorld

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,59 @@
 import greenfoot.*;
-
+/**
+ * The World our hero lives in. 
+ * 
+ * @author Carmen Cheung
+ * @version Apr 28, 2025
+ */
 public class MyWorld extends World {
+    public int score =0;
+    Label scoreLabel;
+    int level = 1; 
     public MyWorld() {
-        super(600, 400, 1);
+        super(600, 400, 1, false);
+        //Create the elephant object
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 300);  
+        
+        //Create a label
+        scoreLabel = new Label(0,80);
+        addObject(scoreLabel, 50, 50); 
+        
+        createPizza();
+        setBackground(new GreenfootImage("background.jpg"));
+    }
+    
+    /** 
+     * End the game and draw "Game Over"
+     */
+    public void gameOver()
+    {
+        Label gameOverLabel = new Label("Game Over", 100);
+        addObject(gameOverLabel, 300, 200); 
+    }
+    
+    /**
+     * Increase score
+     */
+    public void increaseScore()
+    {
+        score++;
+        scoreLabel.setValue(score);
+        
+        if (score % 5 == 0)
+        {
+            level += 1; 
+        }
+    }
+    /**
+     * Create an new Pizza at random location at top of screen
+     */
+    public void createPizza()
+    {
+        Pizza Pizza = new Pizza();
+        Pizza.setSpeed(level); 
+        int x = Greenfoot.getRandomNumber(600); 
+        int y = 0;
+        addObject(Pizza, x, y);
     }
 }
