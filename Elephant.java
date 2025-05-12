90be0d9346a414ac8a70298616e358b92fe360c3
senaG1 <161520172+senaG1@users.noreply.github.com>
2025-05-12 14:22:54 -0400

    updates elephant actor

--- Elephant.java
+++ Elephant.java
@@ -14,6 +14,7 @@ public class Elephant extends Actor
     
     //Direction the elephant is facing
     String facing = "right";
+    SimpleTimer animationTimer = new SimpleTimer();
     /**
     * Constructor - The code that gets run   
     */
@@ -32,11 +33,66 @@ public class Elephant extends Actor
             idleLeft[i].scale(100,100);
         }
         
-  
+        animationTimer.mark();
         
         //initial elephant image
         setImage(idleRight[0]);
     }
     
-
+    /**
+    * Animate the elephant   
+    */
+   
+    int imageIndex = 0;
+    public void animateElephant()
+    {
+        if(animationTimer.millisElapsed() < 100)
+        {
+            return;
+        }
+        animationTimer.mark();
+        
+        if(facing.equals("right"))
+        {
+            setImage(idleRight[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleRight.length;
+        }
+        else
+        {
+            setImage(idleLeft[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleLeft.length;
+        }
+    }
+    
+    public void act()
+    {
+        // Add your action code here.
+        if(Greenfoot.isKeyDown("left")){
+            move(-2);
+            facing = "left";
+        }
+        else if(Greenfoot.isKeyDown("right")){
+            move(2);
+            facing = "right";
+        }
+        
+        //remove Pizza of elephant eatsit
+        eat();
+        
+        // Animate the elephant
+        animateElephant();
+    }
+    
+    /**
+    * Eats the Pizza and spawn new Pizza if an Pizza is eaten
+    */
+    public void eat(){
+        if(isTouching(Pizza.class)){
+            removeTouching(Pizza.class);
+            MyWorld world = (MyWorld) getWorld();
+            world.createPizza();
+            world.increaseScore();
+            elephantSound.play();
+        }    
+    }
 }
