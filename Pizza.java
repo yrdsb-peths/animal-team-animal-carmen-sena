f30740df1592fc932889aa32fdcc29c72858c5f4
CC-iam <1202manman@gmail.com>
2025-05-12 14:14:53 -0400

    Add pizza

---
+++ Pizza.java
@@ -0,0 +1,37 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Food for our elephant.
+ * 
+ * @author Carmen Cheung 
+ * @version May 12, 2025
+ */
+public class Pizza extends Actor
+{
+    /**
+     * Act - do whatever the pizza wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    int speed = 1; 
+    public void act()
+    {
+        GreenfootImage image = new GreenfootImage("pizza2.png");
+        image.scale(50, 50);
+        setImage(image); 
+        //Let the pizza fall.
+        setLocation(getX(), getY() + 1);
+        
+        //Remove pizza and draw the game Over when pizza gets to bottom
+        MyWorld world = (MyWorld)getWorld();
+        if(getY() >= world.getHeight())
+        {
+            world.gameOver();
+            world.removeObject(this); 
+        }
+    }
+    
+    public void setSpeed (int spd)
+    {
+        speed = spd; 
+    }
+}
